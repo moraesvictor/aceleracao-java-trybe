a0987901274b7fe80208a6a42291951fa5fb909d
moraesvictor <moraesvictorgk@gmail.com>
2023-08-25 23:23:36 -0300

    criar classe com método de cálculo de raiz quadrada

---
+++ classes/RaizQuadrada.java
@@ -0,0 +1,11 @@
+package classes;
+
+import java.lang.Math;
+
+public class RaizQuadrada {
+    public static void main(int valor) {
+        double resultado = Math.sqrt(valor);
+
+        System.out.println(resultado);
+    }
+}
