4c764c7c710ef0f1f06a4abe48719374d5f28fea
moraesvictor <moraesvictorgk@gmail.com>
2023-08-25 23:47:48 -0300

    usar classe scanner

---
+++ classes/UsandoScanner.java
@@ -0,0 +1,11 @@
+package classes;
+import java.util.Scanner;
+
+public class UsandoScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual o seu nome? ");
+        String nome = scanner.next();
+        System.out.println("Boas vindas" + nome + "!");
+    }
+}
