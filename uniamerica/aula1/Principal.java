6d6f6e70624f968f1bb77f6efc51c39bc5976b5e
moraesvictor <moraesvictorgk@gmail.com>
2023-08-28 12:09:12 -0300

    add classes

---
+++ uniamerica/aula1/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        String nome = "Victor";
+        String sobrenome = new String("Moraes");
+
+        System.out.println(nome);
+        System.out.println(sobrenome);
+    }
+}
