e9d89fc10587042ea2c8ecc3d027f0dcede59f48
moraesvictor <moraesvictorgk@gmail.com>
2023-08-28 18:50:53 -0300

    nome de guerra

--- uniamerica/aula1/Principal.java
+++ uniamerica/aula1/Principal.java
@@ -1,9 +1,17 @@
 public class Principal {
-    public static void main(String[] args) {
-        String nome = "Victor";
-        String sobrenome = new String("Moraes");
 
-        System.out.println(nome);
-        System.out.println(sobrenome);
+  public static void main(String[] args) {
+    String nome = "Victor";
+    String sobrenome = new String("Moraes");
+    String nomeDeGuerra = new String("Dona Valkyria");
+
+    System.out.println(nome);
+    System.out.println(sobrenome);
+
+    if (nome.equals(nomeDeGuerra)) {
+      System.out.println("Você não tem nome de guerra");
+    } else {
+      System.out.println("Seu nome de guerra é: " + nomeDeGuerra);
     }
+  }
 }
