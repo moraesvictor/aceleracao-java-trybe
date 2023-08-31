ea56d84f0d264b54dbe226001dfad03ad81c99ef
moraesvictor <moraesvictorgk@gmail.com>
2023-08-31 01:07:40 -0300

    stringBuffer

---
+++ uniamerica/aula2/Buffer.java
@@ -0,0 +1,13 @@
+public class Buffer {
+
+  public static void main(String[] args) {
+    String nome = "Victor ";
+    String sobrenome = "Moraes";
+    String nomeCompleto;
+
+    StringBuffer buffer = new StringBuffer();
+
+    buffer.append(nome);
+    buffer.append(sobrenome);
+  }
+}
